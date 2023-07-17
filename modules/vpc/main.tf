e754b5b3fc6268f733cf97d0abca0b09981f19b8
chidinmax <mediumnova28@gmail.com>
2023-07-16 21:38:53 -0600

    updated main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -73,48 +73,48 @@ resource "aws_route_table_association" "public_subnet_az2_route_table_associatio
 
 # create private app subnet az1
 resource "aws_subnet" "private_app_subnet_az1" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = data.aws_availability_zones.available_zones[2]
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.private_app_subnet_az1_cidr
+  availability_zone        = data.aws_availability_zones.available_zones.names[0]
   map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = 
+    Name    = "private app subnet az1"
   }
 }
 
 # create private app subnet az2
 resource "aws_subnet" "private_app_subnet_az2" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = data.aws_availability_zones.available_zones[3]
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.private_app_subnet_az2_cidr
+  availability_zone        = data.aws_availability_zones.available_zones.names[1]
   map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = 
+    Name    = "private app subnet az2"
   }
 }
 
 # create private data subnet az1
 resource "aws_subnet" "private_data_subnet_az1" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.private_data_subnet_az1_cidr
+  availability_zone        = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = 
+    Name    = "private_data_subnet_az1"
   }
 }
 
 # create private data subnet az2
 resource "aws_subnet" "private_data_subnet_az2" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.private_data_subnet_az2_cidr
+  availability_zone        = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = 
+    Name    = "private_data_subnet_az2"
   }
 }
