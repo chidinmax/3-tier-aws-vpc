fcadf9f1d5bbd0bc6328ce074b6ceac964d7a3e5
chidinmax <mediumnova28@gmail.com>
2023-07-16 20:49:46 -0600

    updated main.tf and var.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,6 @@
+variable "region" {}
+variable "project-name" {}
+variable "vpc-cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_subnet_az1_cidr" {}
